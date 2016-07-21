dd1580dad4831c341428bc2cd5d41a8dd6a20573
ranapnm111 <ranapnm@gmail.com>
2016-07-21 17:12:32 +0530

    @7th

--- Animated Table/AppDelegate.m
+++ Animated Table/AppDelegate.m
@@ -15,7 +15,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.dfdfdfddfdfddfadfdfdfffdffffffdadf asfaffqfqfqeqfsfdfddfdf
+    // Override point for customization after application launch.dfdfdfddfdfddfadfdfdfffdffffffdadf asfaffqfqfqeqfsfdfddfdfdasdas
     self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     
     UINavigationController *newNC = [[UINavigationController alloc] initWithRootViewController:self.viewController];
